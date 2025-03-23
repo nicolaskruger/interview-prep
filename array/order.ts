d44ec6ab98fdadb4b3d64133d333eb0ca16c41db
nkruger <nicolas.nkruger@gmail.com>
2025-03-23 07:50:35 -0300

    ✨ feat: order

--- array/order.ts
+++ array/order.ts
@@ -2,40 +2,42 @@ function minimumBribes(q: number[]): void {
   // Write your code here
   const smarts = q.map((_) => 0);
   const c = console;
-
-  const badPeoples = (): boolean => smarts.some((sheet) => sheet > 2);
-  const order = (q: number[]): number => {
-    const notCool = (i: number) => {
-      smarts[q[i] - 1]++;
-      return q;
-    };
-    const swap = (i: number): number[] => {
-      const hold = q[i];
-      q[i] = q[i + 1];
-      q[i + 1] = hold;
-      return q;
-    };
-    const isOrder = () => q.every((v, i) => v - 1 === i);
-    const isNotOrder = () => !isOrder();
-
-    let count = 0;
-
-    for (let j = 0; j < q.length - 1; j++) {
-      const ij = () => j;
-      if (q[ij()] > q[ij() + 1]) {
-        count++;
-        notCool(ij());
-        swap(ij());
-        j = -1;
-      }
-      if (isNotOrder() && j == q.length - 2) j = -1;
-    }
-    return count;
+  let count = 0;
+  const cp = q.map((_, i) => i + 1);
+  const swap = (i: number): number[] => {
+    const hold = cp[i];
+    cp[i] = cp[i + 1];
+    cp[i + 1] = hold;
+    return cp;
   };
-  const times = order(q);
-  if (badPeoples()) c.log("Too chaotic");
-  else c.log(times);
+  for (let i = 0; i < q.length - 1; i++) {
+    const offset = () => q[i] - 1 - i;
+    const smartJumps = () => smarts[q[i] - 1];
+    const calc = () => offset() + smartJumps();
+    const idx = (n: number) => cp[i + n] - 1;
+    const nop = () => {};
+    const one = () => {
+      swap(i);
+      smarts[idx(1)]++;
+      count++;
+    };
+    const two = () => {
+      swap(i + 1);
+      swap(i);
+      smarts[idx(1)]++;
+      smarts[idx(2)]++;
+      count += 2;
+    };
+    const operate: (() => void)[] = [nop, one, two];
+    if (calc() > 2) {
+      c.log("Too chaotic");
+      return;
+    }
+    operate[calc() <= 0 ? 0 : calc()]();
+  }
+  c.log(count);
 }
 
-// minimumBribes([2, 1, 5, 3, 4]); // 3
+minimumBribes([2, 1, 5, 3, 4]); // 3
 minimumBribes([2, 5, 1, 3, 4]); // Too chaotic
+minimumBribes([1, 2, 5, 3, 7, 8, 6, 4]); // 7
