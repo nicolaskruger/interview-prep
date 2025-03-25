3eb58d94d0c8d05b0c75498a5e90d25b98b0cc09
nkruger <nicolas.nkruger@gmail.com>
2025-03-25 06:15:54 -0300

    ✨ feat: jdi

--- array/query.ts
+++ array/query.ts
@@ -3,45 +3,96 @@ function arrayManipulation(n: number, queries: number[][]): number {
   type Query = [number, number, number];
   const split = (queries: Query[], query: Query): Query[] => {
     const [a, b, k] = query;
-    queries.map((q) => {
-      const [x, y, z] = q;
-      if (a === x && b === y) return [a, b, z + k];
-      if (a > x && b > y)
-        return [
-          [a, x - 1, k],
-          [x, y, z + k],
-          [y + 1, b, k],
-        ];
-    });
-    throw new Error("Invalid query");
+    return queries
+      .map((q) => {
+        const [x, y, z] = q;
+        if (a === x && b === y) return [[a, b, z + k]];
+        if (a > x && b > y)
+          return [
+            [a, x - 1, k],
+            [x, y, z + k],
+            [y + 1, b, k],
+          ];
+        if (x > a && y > b)
+          return [
+            [x, a - 1, z],
+            [a, y, z + k],
+            [y + 1, b, z],
+          ];
+        if (a > x && b < y)
+          return [
+            [a, x - 1, k],
+            [x, b, z + k],
+            [b + 1, y, z],
+          ];
+        if (x > a && y < b)
+          return [
+            [x, a - 1, z],
+            [a, y, z + k],
+            [y + 1, b, z],
+          ];
+        if (a > x && y > b)
+          return [
+            [a, x - 1, k],
+            [x, b, z + k],
+            [b + 1, y, z],
+          ];
+        if (a === x && b > y)
+          return [
+            [a, b, z + k],
+            [b + 1, y, z],
+          ];
+        if (a === x && y > b)
+          return [
+            [a, y, z + k],
+            [y + 1, b, z],
+          ];
+        if (a > x && b === y)
+          return [
+            [a, x - 1, x],
+            [x, y, z + k],
+          ];
+        if (x > a && b === y)
+          return [
+            [x, a - 1, z],
+            [a + 1, b, z + k],
+          ];
+      })
+      .flat(1) as Query[];
   };
   return Math.max(
-    ...queries.reduce(split, [] as Query[]).map(([_, __, res]) => res)
+    ...queries
+      .reduce(split, [[0, n - 1, 0]] as Query[])
+      .map(([_, __, res]) => res)
   );
 }
 
-// inp [  ]
-// out [  ]
-
-// inp [    ]
+// inp [   ]
 // out  [  ]
 
-// cases tod:
-
 // inp  [  ]
+// out [   ]
+
+// inp [ ] X
+// out [  ]
+
+// inp [  ] X
+// out [ ]
+
+// inp [  ] X
+// out  [  ]
+
+// inp  [  ] X
+// out [ ]
+
+// inp [  ] X
+// out  [  ]
+
+// inp [  ] X
+// out [  ]
+
+// inp [    ] X
+// out  [  ]
+
+// inp  [  ] X
 // out [    ]
-
-// inp [  ]
-// out  [  ]
-
-// inp  [  ]
-// out [ ]
-
-// inp [  ]
-// out  [  ]
-
-// inp [  ]
-// out [ ]
-
-// inp [ ]
-// out [  ]
